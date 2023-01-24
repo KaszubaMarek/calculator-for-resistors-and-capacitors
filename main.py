5cb048343409ab9b2892b0f02eef6a729dc48d3d
KaszubaMarek <koszubamarek82@gmail.com>
2023-01-24 21:50:08 +0100

    create new repository

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# git@github.com:KaszubaMarek/calculator-for-resistors-and-capacitors.git
 from tkinter.messagebox import showinfo
 import tkinter.messagebox
 import tkinter as tk
@@ -14,7 +15,7 @@ class App(ctk.CTk):
 
         # configure window
         self.title("test")
-        self.geometry(f"{1000}x{600}")
+        self.geometry(f"{1000}x{650}")
         # self.resizable(False, False)
 
         self.options = {'padx': 10, 'pady': 10, 'ipadx': 5, 'ipady': 5}
@@ -33,6 +34,26 @@ class App(ctk.CTk):
         self.cap_frame = FrameCapacitor(self)
         self.cap_frame.grid_propagate(False)
         self.cap_frame.grid(column=1, row=1, **self.options)
+
+    def create_toplevel(self, text):
+        window = ctk.CTkToplevel(self)
+        window.geometry("400x200")
+        window.title('Warning')
+
+        # create label on CTkToplevel window
+        label = ctk.CTkLabel(window, text=text, font=('Ubuntu Light', 20), text_color='#E6C36F')
+        label.place(relx=0.5, rely=0.3, anchor='center')
+
+        # create button on CTkToplevel window
+        button = ctk.CTkButton(window,
+                               text='OK',
+                               font=('Ubuntu Light', 17),
+                               width=150, height=40,
+                               fg_color='#F45F09',
+                               hover_color='#986140',
+                               command=window.destroy
+                               )
+        button.place(relx=0.5, rely=0.7, anchor='center')
 
 
 class FrameTHT(ctk.CTkFrame):
@@ -59,10 +80,10 @@ class FrameTHT(ctk.CTkFrame):
         self.label_band_3 = ctk.CTkLabel(self, text='Third Band\n(Optional)', font=('Ubuntu Light', 12, 'bold'))
         self.label_band_3.grid(column=2, row=1)
 
-        self.label_band_4 = ctk.CTkLabel(self, text='Multiplier', font=('Ubuntu Light', 12, 'bold'))
+        self.label_band_4 = ctk.CTkLabel(self, text='Fourth Band\n(Multiplier)', font=('Ubuntu Light', 12, 'bold'))
         self.label_band_4.grid(column=3, row=1)
 
-        self.label_band_1 = ctk.CTkLabel(self, text='Tolerance', font=('Ubuntu Light', 12, 'bold'))
+        self.label_band_1 = ctk.CTkLabel(self, text='Fifth Band\n(eTolerance)', font=('Ubuntu Light', 12, 'bold'))
         self.label_band_1.grid(column=4, row=1)
 
         # create a combobox
@@ -86,7 +107,6 @@ class FrameTHT(ctk.CTkFrame):
                                                **cb_options,
                                                command=self.color_selected_1
                                                )
-        # self.combobox_band_1._entry.configure(readonlybackground='#2B8FDE')
         self.combobox_band_1.grid(column=0, padx=(10, 10), **cb_grid_options)
 
         self.current_var_2 = ctk.StringVar()
@@ -206,16 +226,18 @@ class FrameTHT(ctk.CTkFrame):
 
         if self.selected_code == '4':
             if len(self.selected_colors) < 4:
-                showinfo(title='Error',
-                         message='You must select colours for all activ bands!')
+                # showinfo(title='Error',
+                #          message='You must select colours for all activ bands!')
+                app.create_toplevel(text='You must select colours\nfor all activ bands!')
             else:
                 result: str = calculates.resistance_calculation(self.selected_colors)
                 # update result label
                 self.result_label.configure(text=result)
         else:
             if len(self.selected_colors) < 5:
-                showinfo(title='Error',
-                         message='You must select colours for all activ bands!')
+                # showinfo(title='Error',
+                #          message='You must select colours for all activ bands!')
+                app.create_toplevel(text='You must select colours\nfor all activ bands!')
             else:
                 result: str = calculates.resistance_calculation(self.selected_colors)
                 # update result label
@@ -253,26 +275,115 @@ class FrameTHT(ctk.CTkFrame):
 
 class FrameSMD(ctk.CTkFrame):
     def __init__(self, container):
-        super().__init__(container, height=230, width=500, corner_radius=10)
+        super().__init__(container, height=270, width=500, corner_radius=10)
 
-        frames_name = ctk.CTkLabel(self, text='SMD Resistors', text_color='white', font=('Ubuntu Light', 12))
-        frames_name.grid(column=0, row=0, sticky=tkinter.SW, padx=10)
+        # Create label widget with frame's name
+        frames_name = ctk.CTkLabel(self, text='SMD Resistors', font=('Ubuntu Light', 12))
+        frames_name.grid(column=0, row=0, sticky=ctk.SW, padx=10)
+
+        # create label widget for entry widget
+        label = ctk.CTkLabel(self, text='Insert code resistor', font=('Ubuntu Light', 18))
+        label.grid(column=0, row=1, pady=10, padx=10, sticky=ctk.W)
+
+        # create entry widget
+        code = tk.StringVar()
+        code_entry = ctk.CTkEntry(self,
+                                  textvariable=code,
+                                  height=35,
+                                  fg_color='white',
+                                  text_color='black',
+                                  font=('Ubuntu Light', 18),
+                                  corner_radius=10,
+                                  width=130
+                                  )
+        code_entry.grid(column=1, row=1, padx=10, pady=10, stick=ctk.E)
+
+        # create frame widget for selecting code
+        frame_code_selection = ctk.CTkFrame(self, fg_color='#242424', corner_radius=15, height=90, width=470)
+        frame_code_selection.grid_propagate(False)
+        frame_code_selection.grid(column=0, row=2, pady=10, padx=10, ipady=5, ipadx=5, columnspan=3, sticky=ctk.W)
+
+        # create label widget with frame code selection name
+        self.sc_frame_name = ctk.CTkLabel(frame_code_selection, text='Select code type',
+                                          font=('Ubuntu Light', 12, 'bold'))
+        self.sc_frame_name.grid(column=0, row=0, padx=10)
+
+        # Create radiobutton-s
+        selected = tk.StringVar()
+        selected.set('3 numbers code')
+        code_3_numbers = ctk.CTkRadioButton(frame_code_selection,
+                                            text='3 numbers',
+                                            value='3 numbers code',
+                                            variable=selected
+                                            )
+        code_3_numbers.grid(column=0, row=1, padx=(0, 5), pady=(20, 30))
+        code_4_numbers = ctk.CTkRadioButton(frame_code_selection,
+                                            text='4 numbers',
+                                            value='4 numbers code',
+                                            variable=selected,
+                                            )
+        code_4_numbers.grid(column=1, row=1, padx=(5, 5), pady=(20, 30))
+        code_eia96 = ctk.CTkRadioButton(frame_code_selection,
+                                        text='EIA96',
+                                        value='EIA96 code',
+                                        variable=selected,
+                                     )
+        code_eia96.grid(column=2, row=1, padx=(5, 5), pady=(20, 30))
+        code_below_10 = ctk.CTkRadioButton(frame_code_selection,
+                                           text='below 10Ω',
+                                           value='below 10',
+                                           variable=selected,
+                                           )
+        code_below_10.grid(column=3, row=1, padx=5, pady=(20, 30))
+
+        # Create result frame
+        self.result_frame = ctk.CTkFrame(self, fg_color='#255B12', corner_radius=10, height=50, width=330)
+        self.result_frame.grid_propagate(False)
+        self.result_frame.grid(column=0, row=3, padx=10, pady=(5, 5), columnspan=2, sticky=ctk.W)
+
+        # create label with result frame name
+        self.result_frame_name = ctk.CTkLabel(self.result_frame,
+                                              text='Result',
+                                              font=('Ubuntu Light', 17, 'bold')
+                                              )
+        self.result_frame_name.grid(column=0, row=0, padx=10, sticky=ctk.W)
+
+        # Create label with result
+        self.result_label = ctk.CTkLabel(self.result_frame,
+                                         font=('Ubuntu Light', 18, 'bold'),
+                                         text='',
+                                         width=50
+                                         )
+        self.result_label.grid(column=1, row=1, sticky=ctk.E)
+
+        # Button calculation for THT resistors
+        self.button_calc = ctk.CTkButton(self,
+                                         text='Calculate',
+                                         font=('Ubuntu Light', 17, 'bold'),
+                                         corner_radius=10,
+                                         width=120,
+                                         height=50,
+                                         )
+        self.button_calc.grid(column=2, row=3, padx=10, pady=10, sticky=ctk.E)
 
 
 class FrameCapacitor(ctk.CTkFrame):
     def __init__(self, container):
-        super().__init__(container, height=230, width=440, corner_radius=10)
+        super().__init__(container, height=270, width=440, corner_radius=10)
 
-        frames_name = ctk.CTkLabel(self, text='Capacitors', text_color='white', font=('Ubuntu Light', 12))
+        # Create label with name for
+        frames_name = ctk.CTkLabel(self, text='Capacitors', font=('Ubuntu Light', 12))
         frames_name.grid(column=0, row=0, sticky=tkinter.SW, padx=10)
 
-
-class FrameSelectCodeTHT(ctk.CTkFrame):
-    def __init__(self, container):
-        super().__init__(container, height=90, width=360, corner_radius=10, fg_color='#242424')
-
-        frames_name = ctk.CTkLabel(self, text='Select number of bands', text_color='white', font=('Ubuntu Light', 12))
-        frames_name.grid(column=0, row=0, sticky=tkinter.SW, padx=10)
+        # # create label for entry widget
+        # label_code_selection = ttk.LabelFrame(frame_smd, text='Select code type')
+        # label_code_selection.grid(column=0, row=1, columnspan=4, pady=5, padx=5, ipady=5, ipadx=5)
+        #
+        # # Create Entry Widget
+        #
+        # code = ctk.StringVar()
+        # code_entry = ctk.CTkEntry(self, textvariable=code)
+        # code_entry.grid(column=1, row=0, padx=30, pady=10, columnspan=3, sticky=tk.W)
 
 
 if __name__ == "__main__":
